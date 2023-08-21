04d835d074c9050a88c8222ac1f886faf0e60f12
k-minyoung <alsdudsk12@naver.com>
2023-08-21 21:43:00 +0900

    동적업로드 실습

--- 12_fileupload_new_pr/index.js
+++ 12_fileupload_new_pr/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer')
+const path = require('path')
 const app = express();
 const PORT = 8000;
 
@@ -8,11 +10,38 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//정적파일
+app.use('/uploads', express.static(__dirname + '/uploads'))
+
+//multer
+const storage = multer.diskStorage({
+    destination : (req,file,cb) =>{
+        cb(null, 'uploads/')
+    },
+    filename : (req,file,cb) =>{
+        const ext = path.extname(file.originalname)
+        const newName = path.basename(file.originalname, ext) + Date.now()+ ext
+        cb(null, newName)
+    },
+    
+})
+const limits = {fileSize : 5 * 1024 * 1024}
+
+const upload = multer({storage,limits})
+
 //router
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.post('/upload',upload.array('userfiles'),(req,res)=>{
+    console.log(req.files)
+    res.send(req.files)
+})
 
+
+app.use('*',(req,res)=>{
+    res.render('404')
+})
 //server open
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
